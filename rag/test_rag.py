59cbf139fe39aa107240a043119ff816cab287e1
Brandon <brandontaylor815@protonmail.com>
2023-10-05 11:27:50 -0400

    blocked on langsmith access -- need an invite code

--- rag/test_rag.py
+++ rag/test_rag.py
@@ -25,11 +25,6 @@ import os
 # =================== SETUP =================== #
 
 @pytest.fixture
-def thing():
-    thing = True
-    return thing
-
-@pytest.fixture
 def chain_1():
     # Load environment (API keys)
     load_dotenv()
@@ -48,43 +43,7 @@ def chain_1():
 
 
 @pytest.fixture
-def chain_2(vector_store):
-    # Load environment (API keys)
-    load_dotenv()
-
-    # Define chain_2
-    prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "You are a helpful documentation Q&A assistant, trained to answer"
-                " questions from LangSmith's documentation."
-                " LangChain is a framework for building applications using large language models."
-                "\nThe current time is {time}.\n\nRelevant documents will be retrieved in the following messages."),
-                ("system", "{context}"),
-                ("human","{question}")
-            ]
-        ).partial(time=str(datetime.now()))
-
-    model = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
-    response_generator = (
-        prompt
-        | model
-        | StrOutputParser()
-    )
-
-    chain_2 = (
-        # The runnable map here routes the original inputs to a context and a question dictionary to pass to the response generator
-        {
-            "context": itemgetter("question") | vector_store | (lambda docs: "\n".join([doc.page_content for doc in docs])),
-            "question": itemgetter("question")
-        }
-        | response_generator
-    )
-    
-    return chain_2
-
-
-@pytest.fixture
-def vector_store():
+def chain_2():
     # Load environment (API keys)
     load_dotenv()
 
@@ -111,7 +70,38 @@ def vector_store():
     vectorstore  = Chroma.from_documents(documents, embeddings)
     retriever    = vectorstore.as_retriever(search_kwargs={"k": 4})
 
-    return retriever
+
+    # Define chain_2
+    prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are a helpful documentation Q&A assistant, trained to answer"
+                " questions from LangSmith's documentation."
+                " LangChain is a framework for building applications using large language models."
+                "\nThe current time is {time}.\n\nRelevant documents will be retrieved in the following messages."),
+                ("system", "{context}"),
+                ("human","{question}")
+            ]
+        ).partial(time=str(datetime.now()))
+
+    model = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
+    response_generator = (
+        prompt
+        | model
+        | StrOutputParser()
+    )
+
+    chain_2 = (
+        # The runnable map here routes the original inputs to a context and a question dictionary to pass to the response generator
+        {
+            "context": itemgetter("question") | retriever | (lambda docs: "\n".join([doc.page_content for doc in docs])),
+            "question": itemgetter("question")
+        }
+        | response_generator
+    )
+    
+    return chain_2
+
+
 
 # ============================================= #
 
@@ -119,11 +109,6 @@ def vector_store():
 
 
 # =================== TESTS =================== #
-
-def test_hello_world(thing):
-    print ("-- other text here")
-    assert thing == True
-
 
 def test_name(chain_1):
     # Define input/output
@@ -145,31 +130,65 @@ def test_basic_arithmetic(chain_1):
     assert "12" in output_text.lower()
 
 
-def test_documentation_correctness(chain_2):
-    for tok in chain_2.stream({"question": "How do I log user feedback to a run?"}):
-        print(tok, end="", flush=True)
+#def test_documentation_llm_deterministic(chain_2):
+#    #output = ""
+#    output_text = chain_2.invoke({"question": "How do I log user feedback to a run?"})
+##    for tok in chain_2.stream({"question": "How do I log user feedback to a run?"}):
+##        print(tok, end="", flush=True)
+#
+#    print(output_text)
 
 
+def test_documentation_llm_evaluators(chain_2):
+    # Initiate the LangSmith Client
+    client = Client()
+
+    # Define question and expected output example test set for qa evaluators in LangSmith.
+    examples = [
+        ("What is LangChain?", "LangChain is an open-source framework for building applications using large language models. It is also the name of the company building LangSmith."),
+        ("How might I query for all runs in a project?", "client.list_runs(project_name='my-project-name'), or in TypeScript, client.ListRuns({projectName: 'my-project-anme'})"),
+        ("What's a langsmith dataset?", "A LangSmith dataset is a collection of examples. Each example contains inputs and optional expected outputs or references for that data point."),
+        ("How do I use a traceable decorator?", """The traceable decorator is available in the langsmith python SDK. To use, configure your environment with your API key,\
+    import the required function, decorate your function, and then call the function. Below is an example:
+    ```python
+    from langsmith.run_helpers import traceable
+    @traceable(run_type="chain") # or "llm", etc.
+    def my_function(input_param):
+        # Function logic goes here
+        return output
+    result = my_function(input_param)
+    ```"""),
+        ("Can I trace my Llama V2 llm?", "So long as you are using one of LangChain's LLM implementations, all your calls can be traced"),
+        ("Why do I have to set environment variables?", "Environment variables can tell your LangChain application to perform tracing and contain the information necessary to authenticate to LangSmith."
+         " While there are other ways to connect, environment variables tend to be the simplest way to configure your application."),
+        ("How do I move my project between organizations?", "LangSmith doesn't directly support moving projects between organizations.")
+    ]
+
+    # Create a dataset for LangSmith evaluator using the example QA list from above
+    dataset_name = f"Retrieval QA Questions {str(uuid.uuid4())}"
+    dataset = client.create_dataset(dataset_name=dataset_name)
+    for q, a in examples:
+        client.create_example(inputs={"question": q}, outputs={"answer": a}, dataset_id=dataset.id)
+
+
+    # Run qa evaluators
     eval_config = RunEvalConfig(
         evaluators=["qa"],
         # If you want to configure the eval LLM:
         # eval_llm=ChatAnthropic(model="claude-2", temperature=0)
     )
 
+
+
     """Run the evaluation. This makes predictions over the dataset and then uses the "QA" evaluator to check the correctness on each data point."""
 
-#    _ = await client.arun_on_dataset(
-#        dataset_name=dataset_name,
-#        llm_or_chain_factory=lambda: chain_2,
-#        evaluation=eval_config,
-#    )
+    #_ = await client.arun_on_dataset(
+    _ = client.arun_on_dataset(
+        dataset_name=dataset_name,
+        llm_or_chain_factory=lambda: chain_2,
+        evaluation=eval_config,
+    )
 
 
-#    # Define input/output
-#    input_text  = "What is your name?"
-#    output_text = chain_1.invoke({"text": input_text})
-#    print("Question: " + input_text)
-#    print("Answer:   " + output_text)
 
-    
 
